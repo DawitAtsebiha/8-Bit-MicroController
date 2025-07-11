d0f7a27ca32b9061434db72a7e63cd7d24326cf6
Dawit <dawitatsebiha@gmail.com>
2025-07-10 20:41:00 -0400

    Updated Demo Assembly Files

---
+++ asm/blink.asm
@@ -0,0 +1,8 @@
+        LDA  #$01      ; pattern 0000_0001
+loop:
+        STAA $F0       ; LED on
+        LDA  #$00
+        STAA $F0       ; LED off
+        BRA  loop      ; forever
+
+        BRA *
