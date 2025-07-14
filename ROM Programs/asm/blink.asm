d9036a65c69b83bd9b921dc28cb773668612fcc0
Dawit <dawitatsebiha@gmail.com>
2025-07-14 16:16:31 -0400

    Modified Assembly files to be more efficent

--- ROM Programs/asm/blink.asm
+++ ROM Programs/asm/blink.asm
@@ -1,8 +1,8 @@
-        LDA  #$01      ; pattern 0000_0001 
+        LDA  #$01      ; Load pattern 1 (LED on)
 loop:
-        STAA $F0       ; LED on
-        LDA  #$00
-        STAA $F0       ; LED off
-        BRA  loop      ; forever
-
-        BRA *
+        STAA $F0       ; Output to LED port (on)
+        LDA  #$01      ; Reload A (creates a delay cycle)
+        LDA  #$00     
+        STAA $F0       
+        LDA  #$00   
+        BRA  loop      ; Continue blinking until simulation is terminated
