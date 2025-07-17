141c330f09426122a231afc42b16b01889f9f49f
Dawit <dawitatsebiha@gmail.com>
2025-07-16 21:43:47 -0400

    Refactor ALU operations to improve flag calculations and enhance clarity in logical operations

--- verilog/ALU.sv
+++ verilog/ALU.sv
@@ -12,11 +12,11 @@ module ALU(
         case(ALU_Sel)
             // ADD
             4'd0: begin
-                temp = {1'b0, A} + {1'b0, B};
+                temp = {1'b0, A} + {1'b0, B};                  
                 Result = temp[7:0];
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
-                NZVC[1] = (A[7] & B[7] & ~Result[7]) |           // Overflow
+                NZVC[2] = ~|Result;                             // Zero
+                NZVC[1] = (A[7] & B[7] & ~Result[7]) |          // Overflow
                          (~A[7] & ~B[7] & Result[7]);
                 NZVC[0] = temp[8];                              // Carry
             end
@@ -26,45 +26,45 @@ module ALU(
                 temp = {1'b0, A} - {1'b0, B};
                 Result = temp[7:0];
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
-                NZVC[1] = (A[7] & ~B[7] & ~Result[7]) |          // Overflow
+                NZVC[2] = ~|Result;                             // Zero
+                NZVC[1] = (A[7] & ~B[7] & ~Result[7]) |         // Overflow
                          (~A[7] & B[7] & Result[7]);
                 NZVC[0] = temp[8];                              // Borrow
             end
             
             // Logical AND
             4'd2: begin
-                Result = A && B;
+                Result = A != 0 && B != 0 ? 8'h01 : 8'h00; 
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
+                NZVC[2] = ~|Result;                             // Zero
             end
             
             // Logical OR
             4'd3: begin
-                Result = A || B;
+                Result = A != 0 || B != 0 ? 8'h01 : 8'h00;
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
+                NZVC[2] = ~|Result;                             // Zero
             end
 
             // Bitwise AND
             4'd4: begin
                 Result = A & B;
-                NZVC[3] = Result[7];     // Negative
-                NZVC[2] = (Result == 0); // Zero
+                NZVC[3] = Result[7];                           // Negative
+                NZVC[2] = ~|Result;                             // Zero
             end
 
             // Bitwise OR
             4'd5: begin
                 Result = A | B;
-                NZVC[3] = Result[7];     // Negative
-                NZVC[2] = (Result == 0); // Zero
+                NZVC[3] = Result[7];                           // Negative
+                NZVC[2] = ~|Result;                             // Zero
             end
 
             // XOR
             4'd6: begin
                 Result = A ^ B;
-                NZVC[3] = Result[7];     // Negative
-                NZVC[2] = (Result == 0); // Zero
+                NZVC[3] = Result[7];                           // Negative
+                NZVC[2] = ~|Result;                             // Zero
             end
             
             // INC (A + 1)
@@ -72,7 +72,7 @@ module ALU(
                 temp = {1'b0, A} + 9'b1;
                 Result = temp[7:0];
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
+                NZVC[2] = ~|Result;                             // Zero
                 NZVC[1] = (A == 8'h7F);                         // Overflow
                 NZVC[0] = temp[8];                              // Carry
             end
@@ -81,7 +81,7 @@ module ALU(
                 temp = {1'b0, B} + 9'b1;
                 Result = temp[7:0];
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
+                NZVC[2] = ~|Result;                             // Zero
                 NZVC[1] = (B == 8'h7F);                         // Overflow
                 NZVC[0] = temp[8];                              // Carry
             end
@@ -90,8 +90,8 @@ module ALU(
                 temp = {1'b0, A} - 9'b1;
                 Result = temp[7:0];
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
-                NZVC[1] = (A == 8'h80);                         // Overflow
+                NZVC[2] = ~|Result;                             // Zero
+                NZVC[1] = (A == 8'h00);                         // Overflow
                 NZVC[0] = temp[8];                              // Borrow
             end           
             // DEC (B - 1)
@@ -99,8 +99,8 @@ module ALU(
                 temp = {1'b0, B} - 9'b1;
                 Result = temp[7:0];
                 NZVC[3] = Result[7];                            // Negative
-                NZVC[2] = (Result == 0);                         // Zero
-                NZVC[1] = (B == 8'h80);                         // Overflow
+                NZVC[2] = ~|Result;                             // Zero
+                NZVC[1] = (B == 8'h00);                         // Overflow
                 NZVC[0] = temp[8];                              // Borrow
             end
             
