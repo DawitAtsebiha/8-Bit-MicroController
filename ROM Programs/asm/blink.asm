bb627758f4100dc59f7ab184b627fa914628f021
Dawit <dawitatsebiha@gmail.com>
2025-07-19 14:45:52 -0400

    Removed obsolete assembly test files that used the old ISA and implemented new ones for testing

--- ROM Programs/asm/blink.asm
+++
@@ -1,7 +0,0 @@
-        LDA  #$00      ; Load pattern 1 (LED OFF)
-loop:
-        STAA $F0       ; Output to LED port
-        LDA  #$01      ; Reload A (creates a delay cycle)
-        STAA $F0       
-        LDA  #$00   
-        BRA  loop      ; Continue blinking until simulation is terminated
--- ROM Programs/asm/counter.asm
+++
@@ -1,9 +0,0 @@
-LDA  #$10     ; A = 16
-LDAB #$00     ; B = 0
-
-loop:
-    DECA      ; A = A + 1
-    INCB      ; B = B + 1
-    STAA $F0  ; Output A value
-    STAB $F0  ; Output B value
-    BRA loop  ; Go back to loop
