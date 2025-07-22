f218626817f1e5d63e76fc8fb22516688a7c200c
Dawit <dawitatsebiha@gmail.com>
2025-07-22 19:20:58 -0400

    Remove test_simple_branch.asm as it is no longer needed; accidentally commited

--- Programs/asm/test_simple_branch.asm
+++
@@ -1,9 +0,0 @@
-// Minimal branch test
-LD A, #$42        // Load A with 0x42 (sets A=42)
-
-LOOP:
-BRA LOOP          // Should jump back to LOOP infinitely
-
-// Expected behavior: Program should loop infinitely at LOOP
-// If working: A=42, PC jumps between BRA instruction and LOOP
-// If broken: A=42, but PC goes to wrong address
